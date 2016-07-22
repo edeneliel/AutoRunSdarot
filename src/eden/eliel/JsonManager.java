9c7945da982c2f1c59e98f52546eaea187fd4743
Eden <2xEshocK@gmail.com>
2016-07-22 19:25:02 +0300

    rearranged all classes so main wont do all the work

--- src/eden/eliel/JsonManager.java
+++ src/eden/eliel/JsonManager.java
@@ -1,34 +1,33 @@
 package eden.eliel;
 
 import com.google.gson.Gson;
-import com.google.gson.GsonBuilder;
 
 import java.io.*;
 import java.util.Map;
 
 public class JsonManager {
-    Gson gson;
-    Map<String,String> map;
-    String file;
+    Gson _gson;
+    Map<String,String> _map;
+    String _file;
 
     public JsonManager(String file) {
-        gson = new Gson();
-        this.file = file;
+        _gson = new Gson();
+        _file = file;
         try {
-            map = gson.fromJson(new FileReader(file),Map.class);
+            _map = _gson.fromJson(new FileReader(_file),Map.class);
         } catch (FileNotFoundException e) {
             e.printStackTrace();
         }
     }
 
     public Object getByKey(String key){
-        return map.get(key);
+        return _map.get(key);
     }
 
     public void setByID(String key,String value){
-        map.put(key,value);
-        try (Writer writer = new FileWriter(file)) {
-            gson.toJson(map, writer);
+        _map.put(key,value);
+        try (Writer writer = new FileWriter(_file)) {
+            _gson.toJson(_map, writer);
         } catch (IOException e) {
             e.printStackTrace();
         }
--- src/eden/eliel/Main.java
+++ src/eden/eliel/Main.java
@@ -1,92 +1,11 @@
 package eden.eliel;
 
-import org.openqa.selenium.By;
-import org.openqa.selenium.JavascriptExecutor;
-import org.openqa.selenium.WebDriver;
-import org.openqa.selenium.WebElement;
-import org.openqa.selenium.chrome.ChromeDriver;
-
-import java.util.ArrayList;
-
 public class Main {
 
-    public static <T> T coalesce(T a, T b){
-        return a == null? b: a;
-    }
-    public static void removeAds(WebDriver webDriver,JavascriptExecutor js){
-        String firstDivId = webDriver.findElement(By.xpath("(//body/div)[1]")).getAttribute("id");
-        if (!firstDivId.equals("fb-root")){
-            js.executeScript("document.getElementById('" + firstDivId + "').remove()");
-        }
 
-        ArrayList <WebElement> app_ads = new ArrayList<WebElement>(webDriver.findElements(By.xpath("//body/*[@type='application/x-shockwave-flash']")));
-        for (WebElement app_ad:app_ads){
-            js.executeScript("document.getElementById('" + app_ad.getAttribute("id") + "').remove()");
-        }
-    }
-    public static void playVideo(JavascriptExecutor js){
-        js.executeScript("jwplayer().play()");
-        js.executeScript("jwplayer().setFullscreen()");
-        js.executeScript("document.getElementById('details').setAttribute('style', 'display: none')");
-    }
 
     public static void main(String[] args) throws InterruptedException {
-        JsonManager jm = new JsonManager("config.json");
-
-        Boolean flag,needRefresh;
-        String url = (String) jm.getByKey("Url");
-        int season = Integer.parseInt(coalesce((String) jm.getByKey("Season"),"1"));
-        int episode = Integer.parseInt(coalesce((String) jm.getByKey("Episode"),"1"));
-        int maxSeason, maxEpisode;
-
-        System.setProperty("webdriver.chrome.driver", "chromedriver.exe");
-        WebDriver webDriver = new ChromeDriver();
-        JavascriptExecutor js = (JavascriptExecutor) webDriver;
-        webDriver.manage().window().maximize();
-
-        webDriver.get(url);
-        maxSeason = Integer.parseInt(webDriver.findElement(By.xpath("(//body//*[@id='season'])/*[last()]")).getAttribute("data-season"));
-
-        for (; season <= maxSeason; season++) {
-            webDriver.get(url + "/season/" + season);
-            maxEpisode = Integer.parseInt(webDriver.findElement(By.xpath("(//body//*[@id='episode'])/*[last()]")).getAttribute("data-episode"));
-            for (; episode <= maxEpisode; episode++) {
-                webDriver.get(url + "/season/" + season + "/episode/" + episode);
-                needRefresh = true;
-
-                while (needRefresh) {
-                    flag = false;
-                    while (!flag) {
-                        flag = (webDriver.findElement(By.id("proceed")).getAttribute("style").equals("display: inline-block;"));
-                        if (webDriver.findElement(By.xpath("//body/div[@class='container']/*[@id='loading']//h1")).getText().equals("שגיאה!"))
-                            webDriver.navigate().refresh();
-                        if (!flag)
-                            Thread.sleep(2000);
-                    }
-                    removeAds(webDriver, js);
-
-                    js.executeScript("scroll(0,250)");
-                    webDriver.findElement(By.id("proceed")).click();
-
-                    if (js.executeScript("return jwplayer().getState()").equals("error"))
-                        webDriver.navigate().refresh();
-                    else
-                        needRefresh = false;
-                }
-                playVideo(js);
-
-                flag = false;
-                while (!flag) {
-                    flag = (js.executeScript("return jwplayer().getState()").equals("complete"));
-                    if (!flag)
-                        Thread.sleep(2000);
-                }
-                jm.setByID("Episode", episode+1+"");
-            }
-            episode = 1;
-            jm.setByID("Episode", episode+"");
-            jm.setByID("Season", season+"");
-        }
-        webDriver.close();
+        AutoSdarot autoSdarot = new AutoSdarot();
+        autoSdarot.execute();
     }
 }
