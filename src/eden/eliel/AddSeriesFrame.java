0b5f124783b72905371b18240e6c2dbac59b74f7
Eden <2xEshocK@gmail.com>
2016-08-01 20:56:12 +0300

    Now update after adding series

--- src/eden/eliel/AddSeriesFrame.java
+++ src/eden/eliel/AddSeriesFrame.java
@@ -4,6 +4,8 @@ import javax.swing.*;
 import java.awt.*;
 import java.awt.event.ActionEvent;
 import java.awt.event.ActionListener;
+import java.awt.event.WindowAdapter;
+import java.awt.event.WindowEvent;
 import java.util.ArrayList;
 
 /**
@@ -19,6 +21,12 @@ public class AddSeriesFrame extends JFrame {
 
     public AddSeriesFrame(Application app){
         _application = app;
+
+        addWindowListener(new WindowAdapter(){
+            public void windowClosing(WindowEvent e) {
+                app.updateSeries();
+            }
+        });
 
         setLayout(new BoxLayout(getContentPane(),BoxLayout.PAGE_AXIS));
         getRootPane().setBorder(BorderFactory.createEmptyBorder(5,5,5,5));
