311cb0a2ae725e50a35d8f437ef202f37fccb549
Eden <2xEshocK@gmail.com>
2016-10-24 01:43:06 +0300

    made it fullscreen, update MAL now

--- src/eden/eliel/AutoAnimeTake.java
+++ src/eden/eliel/AutoAnimeTake.java
@@ -15,6 +15,7 @@ public class AutoAnimeTake {
     private JavascriptExecutor _js;
     private JsonManager _jm;
     private String _seriesId;
+    private String _malSeriesId;
     private int _currentEpisode;
 
     public AutoAnimeTake(JsonManager jsonManager){
@@ -41,20 +42,31 @@ public class AutoAnimeTake {
         maxEpisode = getEpisodesLength();
         for (; _currentEpisode <= maxEpisode; _currentEpisode++) {
             _webDriver.get(DEFAULT_WATCH_URL + "/watch/" + _seriesId + "-episode-" + _currentEpisode);
-            _js.executeScript("player.play()");
+            playVideo();
 
             while (!_js.executeScript("return player.ended()").toString().equals("true"))
                 Thread.sleep(2000);
-            //updateJson(seriesName,_currentEpisode+1);
+            updateJson(seriesName,_currentEpisode+1);
+            MyAnimeListApi.updateMalSeries(_malSeriesId,"episode="+_currentEpisode);
         }
         _webDriver.close();
     }
+
+    private void playVideo(){
+        _js.executeScript("player.play()");
+        _js.executeScript("document.getElementById('video').setAttribute('style', 'height: 1080px!important; width: 1920px!important')");
+        _js.executeScript("scroll(1000,500)");
+    }
     private void setSeries(String seriesName){
         _seriesId = _jm.getKeyBySeries(seriesName,"Id");
+        _malSeriesId = _jm.getKeyBySeries(seriesName,"MAL");
         _currentEpisode = Integer.parseInt(_jm.getKeyBySeries(seriesName,"Episode"));
     }
     private int getEpisodesLength(){
         _webDriver.get(DEFAULT_WATCH_URL + "/anime/" + _seriesId);
         return _webDriver.findElements(By.xpath("(//body//*[@class='no-bullet'])[1]/*")).size();
+    }
+    private void updateJson(String seriesName, int episode){
+        _jm.setKeyBySeries(seriesName,"Episode", episode+"");
     }
 }
--- src/eden/eliel/Main.java
+++ src/eden/eliel/Main.java
@@ -6,7 +6,5 @@ package eden.eliel;
 public class Main {
     public static void main(String [] args) throws InterruptedException {
         Application app = new Application();
-        /*AutoAnimeTake a = new AutoAnimeTake();
-        a.execute("");*/
     }
 }
