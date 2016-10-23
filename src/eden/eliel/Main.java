d10f6dcfb2c0370fc6719a864870ce22244b7939
Eden <2xEshocK@gmail.com>
2016-10-24 02:14:23 +0300

    No need for exception throwing

--- src/eden/eliel/Main.java
+++ src/eden/eliel/Main.java
@@ -4,7 +4,7 @@ package eden.eliel;
  * Created by Eden on 7/28/2016.
  */
 public class Main {
-    public static void main(String [] args) throws InterruptedException {
+    public static void main(String [] args){
         Application app = new Application();
     }
 }
